ba496482203736c40d1231f2f9e2b7f2fc27a100
med96 <davmartinez@urbano.com.ar>
2023-04-27 15:45:22 -0300

    funciona pero con tablas OR, hay que arreglar lo que sucede cuando se quiere hacer una tabla AND y todo converge a cero

--- js/mednet.js
+++ js/mednet.js
@@ -43,6 +43,7 @@ class MedNet {
     this.activationFn = activationFn;
     this.derivativeFn = derivativeFn;
     this.weightInitFn = weightInitFn;
+    this.structure = structure;
 
     var i, j, k;
 
@@ -57,11 +58,13 @@ class MedNet {
           for (k = 0; k < structure[0]; k++) {
             this.layers[i][j].weights[k] = weightInitFn();
             this.layers[i][j].dweights[k] = weightInitFn();
+            // this.layers[i][j].bias = weightInitFn();
           }
         } else {
           for (k = 0; k < structure[i - 1]; k++) {
             this.layers[i][j].weights[k] = weightInitFn();
             this.layers[i][j].dweights[k] = weightInitFn();
+            // this.layers[i][j].bias = weightInitFn();
           }
         }
       }
@@ -86,198 +89,82 @@ class MedNet {
     return this.derivativeFn(x);
   }
 
-  forward() {
-    for (let i = 0; i < this.inputs.length; i++) {
-      for (let j = 0; j < this.layers[0].length; j++) {
-        this.layers[0][j].sum = this.layers[0][j].bias;
-        for (let k = 0; k < this.layers[0][j].weights.length; k++) {
-          this.layers[0][j].sum +=
-            this.inputs[i][k] * this.layers[0][j].weights[k];
-          console.log(
-            `inputs[${i}][${k}]: ${this.inputs[i][k]} | layer[0][${j}]: ${this.layers[0][j].sum}`
-          );
-        }
-        this.layers[0][j].output = this.activationFn(this.layers[0][j].sum);
+  forward(inputs = [0, 0]) {
+    for (let j = 0; j < this.layers[0].length; j++) {
+      this.layers[0][j].sum = this.layers[0][j].bias;
+      for (let i = 0; i < inputs.length; i++) {
+        this.layers[0][j].sum += inputs[i] * this.layers[0][j].weights[i];
       }
+      this.layers[0][j].output = this.activationFn(this.layers[0][j].sum);
+    }
 
-      for (let j = 1; j < this.layers.length; j++) {
-        for (let k = 0; k < this.layers[j].length; k++) {
-          this.layers[j][k].sum = this.layers[j][k].bias;
-          for (let l = 0; l < this.layers[j - 1].length; l++) {
-            this.layers[j][k].sum +=
-              this.layers[j - 1][l].output * this.layers[j][k].weights[l];
-          }
-          this.layers[j][k].output = this.activationFn(this.layers[j][k].sum);
+    for (let j = 1; j < this.layers.length; j++) {
+      for (let k = 0; k < this.layers[j].length; k++) {
+        this.layers[j][k].sum = this.layers[j][k].bias;
+        for (let l = 0; l < this.layers[j - 1].length; l++) {
+          this.layers[j][k].sum += this.layers[j - 1][l].output * this.layers[j][k].weights[l];
         }
+        this.layers[j][k].output = this.activationFn(this.layers[j][k].sum);
       }
     }
+    
   }
 
-  backward() {
+  backward(targets = [0]) {
     var i, j, k, l, m, context, lc;
     context = this;
     lc = context.layers[context.layers.length - 1];
 
-    this.targets.forEach((item, r) => {
+    targets.forEach((item) => {
       for (i = 0; i < lc.length; i++) {
-        let delta = lc[i].output - item[i];
+        let delta = lc[i].output - item;
         lc[i].dsum = context.derivativeFn(lc[i].sum) * delta;
       }
 
-      for (i = context.layers.length - 2; i >= 0; i--) {
-        for (j = 0; j < context.layers[i].length; j++) {
-          context.layers[i][j].doutput = 0;
-          for (k = 0; k < context.layers[i + 1].length; k++) {
-            for (l = 0; l < context.layers[i + 1][k].weights.length; l++) {
-              context.layers[i][j].doutput +=
-                context.layers[i + 1][k].dsum *
-                context.layers[i + 1][k].weights[l];
-            }
-          }
-          context.layers[i][j].dsum =
-            context.derivativeFn(context.layers[i][j].sum) *
-            context.layers[i][j].doutput;
-        }
-      }
-
-      for (i = 1; i < context.layers.length; i++) {
-        for (j = 0; j < context.layers[i].length; j++) {
-          for (k = 0; k < context.layers[i][j].weights.length; k++) {
-            context.layers[i][j].dweights[k] =
-              context.layers[i][j].dweights[k] +
-              context.layers[i - 1][k].output * context.layers[i][j].dsum;
-          }
-          context.layers[i][j].dbias =
-            context.layers[i][j].dbias + context.layers[i][j].dsum;
-        }
-      }
     });
 
-    for (i = 1; i < context.layers.length; i++) {
-      for (j = 0; j < context.layers[i].length; j++) {
-        for (k = 0; k < context.layers[i][j].weights.length; k++) {
-          context.layers[i][j].weights[k] =
-            context.layers[i][j].weights[k] -
-            context.learning_rate * context.layers[i][j].dweights[k];
-          context.layers[i][j].dweights[k] = 0;
+      for (i = this.layers.length - 2; i >= 0; i--) {
+        for (j = 0; j < this.layers[i].length; j++) {
+          this.layers[i][j].doutput = 0;
+          for (k = 0; k < this.layers[i + 1].length; k++) {
+            for (l = 0; l < this.layers[i + 1][k].weights.length; l++) {
+              this.layers[i][j].doutput += this.layers[i + 1][k].dsum * this.layers[i + 1][k].weights[l];
+            }
+          }
+          this.layers[i][j].dsum = this.derivativeFn(this.layers[i][j].sum) * this.layers[i][j].doutput;
         }
-        context.layers[i][j].bias =
-          context.layers[i][j].bias -
-          context.learning_rate * context.layers[i][j].dbias;
-        context.layers[i][j].dbias = 0;
       }
-    }
-  }
 
-  update() {
-    var i, j, k;
+      for (i = 1; i < this.layers.length; i++) {
+        for (j = 0; j < this.layers[i].length; j++) {
+          for (k = 0; k < this.layers[i][j].weights.length; k++) {
+            this.layers[i][j].dweights[k] =
+              this.layers[i][j].dweights[k] +
+              this.layers[i - 1][k].output * this.layers[i][j].dsum;
+          }
+          this.layers[i][j].dbias = this.layers[i][j].dbias + this.layers[i][j].dsum;
+        }
+      }
+  
 
-    for (i = 0; i < this.layers.length; i++) {
+    for (i = 1; i < this.layers.length; i++) {
       for (j = 0; j < this.layers[i].length; j++) {
         for (k = 0; k < this.layers[i][j].weights.length; k++) {
-          this.layers[i][j].weights[k] =
-            this.layers[i][j].weights[k] -
-            this.learning_rate * this.layers[i][j].dweights[k];
+          this.layers[i][j].weights[k] = this.layers[i][j].weights[k] - this.learning_rate * this.layers[i][j].dweights[k];
           this.layers[i][j].dweights[k] = 0;
         }
-        this.layers[i][j].bias =
-          this.layers[i][j].bias - this.learning_rate * this.layers[i][j].dbias;
-        this.layers[i][j].doutput = 0;
-        this.layers[i][j].dsum = 0;
+        this.layers[i][j].bias = this.layers[i][j].bias - this.learning_rate * this.layers[i][j].dbias;
       }
     }
   }
+
+
 
   train(iterations = 1000) {
     for (var o = 0; o < iterations; o++) {
-      var lastLayer = this.layers[this.layers.length - 1];
-      var i, j, k, l, m;
-
-      for (i = 0; i < this.inputs.length; i++) {
-
-        for (j = 0; j < this.layers[0].length; j++) {
-          this.layers[0][j].sum = this.layers[0][j].bias;
-          for (k = 0; k < this.layers[0][j].weights.length; k++) {
-            this.layers[0][j].sum +=
-              this.inputs[i][k] * this.layers[0][j].weights[k];
-            // console.log(`inputs[${i}][${k}]: ${this.inputs[i][k]} | sum layer[0] neuron[${j}]: ${ this.layers[0][j].sum}`);
-          }
-          this.layers[0][j].output = this.activationFn(this.layers[0][j].sum);
-        }
-
-        for (j = 1; j < this.layers.length; j++) {
-          for (k = 0; k < this.layers[j].length; k++) {
-            this.layers[j][k].sum = this.layers[j][k].bias;
-            for (l = 0; l < this.layers[j - 1].length; l++) {
-              this.layers[j][k].sum +=
-                this.layers[j - 1][l].output * this.layers[j][k].weights[l];
-            }
-            this.layers[j][k].output = this.activationFn(this.layers[j][k].sum);
-          }
-        }
-
-        for (j = 0; j < lastLayer.length; j++) {
-          let delta = lastLayer[j].output - this.targets[i][j];
-          lastLayer[j].dsum = this.derivativeFn(lastLayer[j].sum) * delta;
-        }
-
-        for (j = this.layers.length - 2; j >= 0; j--) {
-          for (m = 0; m < this.layers[j].length; m++) {
-            this.layers[j][m].doutput = 0;
-            for (k = 0; k < this.layers[j + 1].length; k++) {
-              for (l = 0; l < this.layers[j + 1][k].weights.length; l++) {
-                this.layers[j][m].doutput +=
-                  this.layers[j + 1][k].dsum * this.layers[j + 1][k].weights[l];
-              }
-            }
-            this.layers[j][m].dsum =
-              this.derivativeFn(this.layers[j][m].sum) *
-              this.layers[j][m].doutput;
-          }
-        }
-
-        for (l = 1; l < this.layers.length; l++) {
-          for (j = 0; j < this.layers[l].length; j++) {
-            for (k = 0; k < this.layers[l][j].weights.length; k++) {
-              this.layers[l][j].dweights[k] =
-                this.layers[l][j].dweights[k] +
-                this.layers[l - 1][k].output * this.layers[l][j].dsum;
-            }
-            this.layers[l][j].dbias =
-              this.layers[l][j].dbias + this.layers[l][j].dsum;
-          }
-        }
-
-        for (l = 1; l < this.layers.length; l++) {
-          for (j = 0; j < this.layers[l].length; j++) {
-            for (k = 0; k < this.layers[l][j].weights.length; k++) {
-              this.layers[l][j].weights[k] =
-                this.layers[l][j].weights[k] -
-                this.learning_rate * this.layers[l][j].dweights[k];
-              this.layers[l][j].dweights[k] = 0;
-            }
-            this.layers[l][j].bias =
-              this.layers[l][j].bias -
-              this.learning_rate * this.layers[l][j].dbias;
-            this.layers[l][j].dbias = 0;
-          }
-        }
-
-        // for (l = 0; l < this.layers.length; l++) {
-        //   for (j = 0; j < this.layers[l].length; j++) {
-        //     for (k = 0; k < this.layers[l][j].weights.length; k++) {
-        //       this.layers[l][j].weights[k] =
-        //         this.layers[l][j].weights[k] -
-        //         this.learning_rate * this.layers[l][j].dweights[k];
-        //       this.layers[l][j].dweights[k] = 0;
-        //     }
-        //     this.layers[l][j].bias =
-        //       this.layers[l][j].bias -
-        //       this.learning_rate * this.layers[l][j].dbias;
-        //     this.layers[l][j].doutput = 0;
-        //     this.layers[l][j].dsum = 0;
-        //   }
-        // }
+      for(var i = 0; i < this.inputs.length; i++) {
+        this.forward(this.inputs[i]);
+        this.backward(this.targets[i]);
       }
     }
   }
@@ -289,26 +176,10 @@ class MedNet {
       );
     }
 
-    var i, j, k, sum;
-
-    for (i = 0; i < inputs.length; i++) {
-      this.layers[0][i].output = inputs[i];
-    }
-
-    for (i = 2; i < this.layers.length; i++) {
-      for (j = 0; j < this.layers[i].length; j++) {
-        sum = this.layers[i][j].bias;
-        for (k = 0; k < this.layers[i - 1].length; k++) {
-          sum += this.layers[i - 1][k].output * this.layers[i][j].weights[k];
-        }
-        this.layers[i][j].sum = sum;
-        this.layers[i][j].output = this.activationFn(sum);
-      }
-    }
-
+    this.forward(inputs);
     var outputLayer = this.layers[this.layers.length - 1];
     var output = new Array(outputLayer.length);
-    for (i = 0; i < outputLayer.length; i++) {
+    for (var i = 0; i < outputLayer.length; i++) {
       output[i] = outputLayer[i].output;
     }
     return output;
@@ -318,16 +189,16 @@ class MedNet {
 function iniciar() {
   // Crear la instancia de la red neuronal
   const net = new MedNet({
-    structure: [2, 3, 1], // 2 nodos de entrada, 2 nodos ocultos y 1 nodo de salida
-    learning_rate: 0.1, // Tasa de aprendizaje
+    structure: [3, 3, 1], // 2 nodos de entrada, 2 nodos ocultos y 1 nodo de salida
+    learning_rate: 0.091, // Tasa de aprendizaje
   });
 
   // Definir el dataset de entrenamiento
   const trainingData = [
-    // { inputs: [0, 0], outputs: [0] },
-    { inputs: [0, 0], outputs: [0] },
-    { inputs: [1, 1], outputs: [1] },
-    // { inputs: [1, 1], outputs: [1] },
+    { inputs: [0, 0, 0], outputs: [0] },
+    { inputs: [0, 0, 1], outputs: [0] },
+    { inputs: [0, 1, 1], outputs: [0] },
+    { inputs: [1, 1, 1], outputs: [1] },
   ];
 
   // Entrenar la red neuronal
@@ -336,8 +207,10 @@ function iniciar() {
   net.train(1000000);
 
   // Evaluar la red neuronal
-  //    console.log(net.output([0,0]));
-  console.log(net.output([1, 1]));
-  console.log(net.output([0, 0]));
-  //    console.log(net.output([1,1]));
+  console.log("[0, 0, 0] = 0: " + net.output([0, 0, 0]));
+  console.log("[0, 0, 1] = 1: " + net.output([0, 0, 1]));
+  console.log("[0, 1, 1] = 1: " + net.output([0, 1, 1]));
+  console.log("[1, 1, 1] = 1: " + net.output([1, 1, 1]));
+
+  console.log(JSON.stringify(net));
 }
